96fb441e0000be361f72969d93b44b1b279ab08f
Gabriel_Neres <sr.neres101@gmail.com>
2026-09-04 10:04:03 -0300

    Adicionando mais conteúdo em Main.java

--- Aula2/src/Main.java
+++ Aula2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Systema de Projetos");
+        System.out.println("===========================");
+        System.out.println("    SYSTEMA DE PROJETOS");
+        System.out.println("============================\n");
+        System.out.println("Bem vindo ao sistema");
     }
 }
